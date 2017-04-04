54f8f98096761336a992e09929ef90ca486c1b1b
seedlcw <seedlcw@gmail.com>
2017-04-04 17:29:01 +0800

    英文報名表

---
+++ apply.eg.js
@@ -0,0 +1,15 @@
+function gotoPage(){
+var eClass = document.getElementById("applyClass");
+var applyForm = document.getElementById("formId");
+var loc;
+var twentyDayEg ="https://goo.gl/c8GwbW";
+var threeDayEg ="https://goo.gl/foHBhC";
+
+        if (eClass.value==1) {
+            // alert("udn.com")
+            loc=threeDayForStudent;
+        // 二十日課
+        } else {
+            loc=twentyDayForStudent;
+        }
+}
